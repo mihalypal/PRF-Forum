a89ea533c08f42bb2f1fa09c2462de732e0d3678
mihalypal <palijan.09@gmail.com>
2024-12-08 17:33:14 +0100

    change main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.0"
+    }
+  }
+}
+
 provider "docker" {}
 
 resource "docker_image" "docker_compose_image" {
