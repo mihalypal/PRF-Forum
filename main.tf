490ba893af78295bfd480d6dc43a4f6ae9d907ca
mihalypal <palijan.09@gmail.com>
2024-12-08 18:16:27 +0100

    never gonna give you up

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "docker_container" "docker_compose" {
   image = docker_image.docker_compose_image.name
   name  = "docker_compose_runner"
   volumes {
-    host_path      = "${path.module}/docker-compose.yml" # Path relative to the main.tf
+    host_path      = "/var/jenkins_home/worksapce/MEAN-Stack-Pipeline/docker-compose.yml" # Path relative to the main.tf
     container_path = "/docker-compose.yml"
   }
   command = ["docker-compose", "-f", "/docker-compose.yml", "up", "--build", "-d"]
