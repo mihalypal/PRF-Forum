bd5eda711e4a1cdbe2477faa26ef90619383e3d7
mihalypal <palijan.09@gmail.com>
2024-12-08 18:23:52 +0100

    terraform szkript update

--- main.tf
+++ main.tf
@@ -9,16 +9,63 @@ terraform {
 
 provider "docker" {}
 
-resource "docker_image" "docker_compose_image" {
-  name = "docker/compose:latest"
+# MongoDB container
+resource "docker_image" "mongodb_image" {
+  name = "my_mongo_image"
+  build {
+    context    = "./backend"
+    dockerfile = "Dockerfile"
+  }
 }
 
-resource "docker_container" "docker_compose" {
-  image = docker_image.docker_compose_image.name
-  name  = "docker_compose_runner"
-  volumes {
-    host_path      = "/var/jenkins_home/worksapce/MEAN-Stack-Pipeline/docker-compose.yml" # Path relative to the main.tf
-    container_path = "/docker-compose.yml"
+resource "docker_container" "mongodb_container" {
+  image = docker_image.mongodb_image.name
+  name  = "my_mongo_container"
+  ports {
+    internal = 27017
+    external = 5000
   }
-  command = ["docker-compose", "-f", "/docker-compose.yml", "up", "--build", "-d"]
+  restart = "always"
+}
+
+# Backend container
+resource "docker_image" "backend_image" {
+  name = "backend-app"
+  build {
+    context    = "./backend"
+    dockerfile = "Dockerfile_be"
+  }
+}
+
+resource "docker_container" "backend_container" {
+  image = docker_image.backend_image.name
+  name  = "backend_container"
+  ports {
+    internal = 3000
+    external = 3000
+  }
+  restart = "always"
+  depends_on = [docker_container.mongodb_container]
+  env = {
+    MONGO_URL = "mongodb://my_mongo_container:27017/my_db"
+  }
+}
+
+# Frontend container
+resource "docker_image" "frontend_image" {
+  name = "frontend-app"
+  build {
+    context    = "./frontend"
+  }
+}
+
+resource "docker_container" "frontend_container" {
+  image = docker_image.frontend_image.name
+  name  = "frontend_container"
+  ports {
+    internal = 4200
+    external = 4200
+  }
+  restart = "always"
+  depends_on = [docker_container.backend_container]
 }
